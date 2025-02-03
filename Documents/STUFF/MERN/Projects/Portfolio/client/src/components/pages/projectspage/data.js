984e48a9d5414a380c81a4bce22f29b3cf5feeb7
Ritesh Thakur <134767629+riteshh-thakur@users.noreply.github.com>
2025-02-04 00:48:13 +0530

    Update data.js

--- Documents/STUFF/MERN/Projects/Portfolio/client/src/components/pages/projectspage/data.js
+++ Documents/STUFF/MERN/Projects/Portfolio/client/src/components/pages/projectspage/data.js
@@ -57,7 +57,7 @@ const projectData = [
         id:10,
         projectName:"Canva-Editor",
         demo:"https://67a1145caabe832ad69ea12b--fantastic-jalebi-d065b4.netlify.app/",
-        imgsrc:"/Screenshot 2025-01-09 141420.png"
+        imgsrc:"/Screenshot 2025-02-04 005301.png"
     },
 ]
 
